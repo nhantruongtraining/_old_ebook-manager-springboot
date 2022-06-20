b9f8c05e261e0f1a1893262dfb1464d366ff2cf8
nhantruongtraining <nhan.truongcongthu@axonactive.com>
2022-06-20 08:16:51 +0700

    add Contributor

--- ebookapp/src/main/java/com/axonactive/training/ebookapp/api/request/ContributorRequest.java
+++ ebookapp/src/main/java/com/axonactive/training/ebookapp/api/request/ContributorRequest.java
@@ -1,13 +1,13 @@
-//package com.axonactive.training.ebookapp.api.request;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class ContributorRequest {
-//    private String ebook;
-//    private String type;
-//}
+package com.axonactive.training.ebookapp.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContributorRequest {
+    private String ebook;
+    private String type;
+}
--- ebookapp/src/main/java/com/axonactive/training/ebookapp/repository/UserEbookRepository.java
+++ ebookapp/src/main/java/com/axonactive/training/ebookapp/repository/UserEbookRepository.java
@@ -1,7 +1,7 @@
-//package com.axonactive.training.ebookapp.repository;
-//
-//import com.axonactive.training.ebookapp.entity.UserEbook;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserEbookRepository extends JpaRepository<UserEbook, Integer> {
-//}
+package com.axonactive.training.ebookapp.repository;
+
+import com.axonactive.training.ebookapp.entity.UserEbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserEbookRepository extends JpaRepository<UserEbook, Integer> {
+}
